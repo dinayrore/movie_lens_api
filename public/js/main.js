4dd2a3bcc7315042f55c22504d18fd6dd4385f7a
Marliana Lara <marliana.lara@gmail.com>
2016-10-12 17:51:01 -0400

    display top rated movies

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,44 @@
-// function getMovie(){
-//   $.ajax({
-//     url: '/api/top_rated_movies',
-//     dataType: 'json',
-//     success: function() {
-//       console.log(data);
-//     }
-//   });
-// }
-// getMovie();
 
-$.get("/api/top_rated_movies", function(data) {
-  console.log('load performed');
+function getMovie() {
+  $.ajax({
+    url: 'http://localhost:9393/api/movies',
+    dataType: 'json',
+    success: function(data) {
+      console.log('yay');
+      return data;
+    },
+    error: function() {
+      console.log('no');
+    }
+  });
+}
+
+$('#movieSearch').on('click', function() {
+  getMovie();
+  // get input on click
+
+  // find movie title based on input
+
+});
+
+$('#home').on('click', function() {
+  console.log("clicked on home");
+});
+$('#genres').on('click', function() {
+  console.log("clicked on genres");
+});
+
+$('#topMovies').on('click', function() {
+  $.get('/api/top_rated_movies', function(data) {
+    var source = $('#movie-template').html();
+    var template = Handlebars.compile(source);
+    data.forEach(function(movieWrapper) {
+      var context = {
+        title: movieWrapper.movie.title,
+        avgRating: movieWrapper.average_rating
+      };
+      var html = template(context);
+      $('.content').prepend(html);
+    });
+  });
 });
