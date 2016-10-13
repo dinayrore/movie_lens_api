775334afef8a093427b27ecda6decaf2b1467d82
Marliana Lara <marliana.lara@gmail.com>
2016-10-13 14:13:10 -0400

    fixed handlebars error with movie search

--- public/js/main.js
+++ public/js/main.js
@@ -1,40 +1,40 @@
 
 (function () {
 
-function getMovie(movieSelection) {
-  $.ajax({
-    url: 'http://localhost:9393/api/movies',
-    data: movieSelection,
-    dataType: 'json',
-    method: 'GET',
+  function getMovie(movieSelection) {
+    $.ajax({
+      url: 'http://localhost:9393/api/movies',
+      data: movieSelection,
+      dataType: 'json',
+      method: 'GET',
 
-    success: function(rawData) {
-      rawData.forEach(function(dataObj) {
-        var movieTitle = dataObj.title.toLowerCase();
+      success: function(rawData) {
+        $('.content-container').empty();
+        rawData.forEach(function(dataObj) {
+          var movieTitle = dataObj.title.toLowerCase();
+          if (movieSelection.toLowerCase() === movieTitle) {
+            console.log(dataObj);
+            var source = $('#movie-template').html();
+            var template = Handlebars.compile(source);
+            var context = {
+              title: dataObj.title
+            };
+            var html = template(context);
+            $('.content-container').prepend(html).fadeIn('slow');
+          }
+        });
+      },
+      error: function() {
+        console.log('no');
+      }
+    });
+  }
 
-        if (movieSelection.toLowerCase() === movieTitle) {
-          console.log(dataObj);
-          var source = $('#movie-template').html();
-          var template = Handlebars.compile(source);
-          var context = {
-            title: dataObj.title
-          };
-          var html = template(context);
-          $('.content-container').prepend(html).fadeIn('slow');
 
-        }
-      });
-    },
-    error: function() {
-      console.log('no');
-    }
+  $('#movieSearch').on('click', function() {
+    var movieSelection = $('.searchField').val();
+    getMovie(movieSelection);
   });
-}
-
-$('#movieSearch').on('click', function() {
-  var movieSelection = $('.searchField').val();
-  getMovie(movieSelection);
-});
 
   function updateHash(hash) {
     window.location.hash = hash;
@@ -47,36 +47,24 @@ $('#movieSearch').on('click', function() {
     $('#movieSearch').fadeIn();
   });
 
-  $('#genres').on('click', function() {
-    updateHash('genres');
-
-  });
-
-
+  // Display Top Rated Movies
   $('nav').on('click', '#top-rated-movies', function() {
     updateHash('top-rated-movies');
-
+    $('input').fadeOut();
+    $('#movieSearch').fadeOut('slow');
+    $('.content-container').fadeOut('slow').empty();
 
     $.get('/api/top_rated_movies', function(data) {
-      console.log(data);
       var source = $('#top-movies-template').html();
       var template = Handlebars.compile(source);
-
       data.forEach(function(movieWrapper) {
         var context = {
           title: movieWrapper.movie.title,
           avgRating: movieWrapper.average_rating.toFixed(1)
         };
-
-        var html = template(context);
-        console.log(data);
-
-        $('input').fadeOut();
-        $('#movieSearch').fadeOut('fast');
-        $('.content-container').prepend(html).fadeIn('slow');
-
+      var html = template(context);
+      $('.content-container').prepend(html).fadeIn('slow');
       });
     });
   });
-
 })();
